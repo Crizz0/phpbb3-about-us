2bb135bf348fd8bcbe03c60924dcb7cb3d5e9419
Crizzo <mail@crizzo.de>
2014-04-17 22:33:14 +0200

    Adds missing ext.php file to github.com repo

---
+++ ext.php
@@ -0,0 +1,18 @@
+<?php
+/**
+*
+* @package phpBB Extension - Crizzo Simple Imprint
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace crizzo\simpleimprint;
+
+/**
+* @ignore
+*/
+
+class ext extends \phpbb\extension\base
+{
+}
