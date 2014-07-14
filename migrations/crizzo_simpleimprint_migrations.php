33c90249a7672eda71ddc109c15a617178062da6
Crizzo <mail@crizzo.de>
2014-07-14 19:15:23 +0200

    Bugfix to template event and composer.json update for RC2

--- migrations/crizzo_simpleimprint_migrations.php
+++ migrations/crizzo_simpleimprint_migrations.php
@@ -7,8 +7,31 @@
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
+namespace crizzo\simpleimprint;
 
-class phpbb_ext_crizzo_simpleimprint_migration extends phpbb_db_migration
+class crizzo_simpleimprint_migration extends \phpbb\db\migration\migration
 {
-    // Data for database changes will go in here!
+	public function effectively_installed()
+	{
+		return isset($this->config['contact_admin_form_enable']);
+	}
+
+	public function update_data()
+	{
+		return array(
+		array('config.add', array('contact_admin_form_enable', 1)),
+		array('custom', array(array($this, 'contact_admin_info'))),
+	);
+	}
+
+	public function contact_admin_info()
+	{
+		$text_config = new \phpbb\config\db_text($this->db, $this->table_prefix . 'config_text');
+		$text_config->set_array(array(
+		'contact_admin_info' => '',
+		'contact_admin_info_uid' => '',
+		'contact_admin_info_bitfield' => '',
+		'contact_admin_info_flags' => OPTION_FLAG_BBCODE + OPTION_FLAG_SMILIES + OPTION_FLAG_LINKS,
+		));
+	}
 }
