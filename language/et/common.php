5a886187f0e3025db5223fe230b2a468a54fa223
phpBBeesti <info@phpbbeesti.com>
2014-09-28 10:27:21 +0300

    ../language/et/common.php
    Estonian translation for About Us-Page extension. (phpBB 3.1)

---
+++ language/et/common.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* ../language/et/common.php
+* @package phpBB Extension - Crizzo About us (Estonian)
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* @copyright (c) 2014 About us Estonian language pack version 0.1 by http://www.phpbbeesti.com/
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ABOUTUS'						=> 'Meist',
+	'ABOUTUS_SETTINGS'				=> 'Lehekülje "Meist" seaded',
+	'ABOUTUS_UPDATED'				=> 'Lehekülg "Meist" on edukalt uuendatud.',
+
+	'ACP_ABOUTUS_ENABLE'			=> 'Meist',
+	'ACP_ABOUTUS_ENABLE_EXPLAIN'	=> 'Kui "Lubatud" kuvab lingi jalusesse ja lehte kasutajatele. Kui "Keelatud", siis lehte ei näidata.',
+	'ACP_ABOUTUS_INFO'				=> 'Lehekülje "Meist" tekst',
+	'ACP_ABOUTUS_INFO_EXPLAIN'		=> 'Sa saad muuta teksti, mida näidatakse lehekülje "Meist" lehel siin.',
+	'ACP_ABOUTUS_INFO_PREVIEW'		=> 'Lehekülje "Meist" eelvaade',
+	'ACP_ABOUTUS_SETTINGS'			=> 'Lehekülje "Meist" seaded',
+	'ACP_ABOUTUS_SETTINGS_EXPLAIN'	=> 'Siin saad muuta oma teksti, mida näidatakse lehel "Meist" või lubada / keelata laienduse',
+
+	'VIEWONLINE_ABOUTUS'	=> 'Vaatab lehekülge "Meist"',
+));
