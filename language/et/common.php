2c93b5a769df1363a68c84c242e05f4f80a3e4af
phpBBeesti <info@phpbbeesti.com>
2014-10-06 10:11:00 +0300

    Version update to 2.0.0-b2

--- language/et/common.php
+++ language/et/common.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* @package phpBB Extension - Crizzo About us (English)
+* @package phpBB Extension - Crizzo About us (Estonian)
 * @copyright (c) 2014 phpBB Group
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
