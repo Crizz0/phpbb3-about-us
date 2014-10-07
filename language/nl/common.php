b75acbee8b5983815c42e505a397106a7107d0fe
Crizzo <mail@crizzo.de>
2014-10-07 18:34:45 +0200

    Little changes in nl/common.php

--- language/nl/common.php
+++ language/nl/common.php
@@ -22,19 +22,18 @@ $lang = array_merge($lang, array(
 	'ABOUTUS'						=> 'Over Ons',
 	'ABOUTUS_SETTINGS'				=> 'Over ons Instellingen',
 	'ABOUTUS_UPDATED'				=> 'Over ons is succesvol bijgewerkt.',
+	'ABOUTUS_AND'					=> 'en',
 
 	'ACP_ABOUTUS_INFO'				=> 'Over ons Tekst',
-	'ACP_ABOUTUS_INFO_EXPLAIN'		=> 'Hier kan je de tekst aanpassen die wordt weergegeven op de "Over ons" pagina.',	
-    'ACP_ABOUTUS_INFO_PREVIEW'		=> 'Over ons voorbeeld',	
+	'ACP_ABOUTUS_INFO_EXPLAIN'		=> 'Hier kan je de tekst aanpassen die wordt weergegeven op de "Over ons"-pagina.',	
+	'ACP_ABOUTUS_INFO_PREVIEW'		=> 'Over ons voorbeeld',
 	'ACP_ABOUTUS_SETTINGS'			=> 'Over ons instellingen',
-	'ACP_ABOUTUS_SETTINGS_EXPLAIN'	=> 'Hier kan je de tekst voor de "Over ons" pagina aanpassen en je kan de "Over ons"-pagina inschakelen of uitschakelen.',
+	'ACP_ABOUTUS_SETTINGS_EXPLAIN'	=> 'Hier kan je de tekst voor de "Over ons"-pagina aanpassen en je kan de "Over ons"-pagina inschakelen of uitschakelen.',
 
 	'ACP_TERMS_OF_USE_ENABLE' 			=> 'Laat voorwaarden voor gebruik zien',
 	'ACP_TERMS_OF_USE_ENABLE_EXPLAIN'	=> 'Laat een link naar de voorwaarden voor gebruik zien op de over ons pagina.',
 	'ACP_PRIVACY_ENABLE' 				=> 'Laat privacybeleid zien',
 	'ACP_PRIVACY_ENABLE_EXPLAIN'		=> 'Laat een link naar het privacybeleid zien op de over ons pagina.',
-
-	'ABOUT_US_AND'					=> 'en',
 
 	'DESCRIPTION_PRIVACY'			=> 'Je kan het privacybeleid hier zien:',
 	'DESCRIPTION_TERMS_OF_USE'		=> 'Je kan de voorwaarden voor gebruik hier zien:',
