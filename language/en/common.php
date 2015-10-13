ba91dcafce17f46fc3961eb8fceead86309e719f
phpBB España <thekuka@gmail.com>
2015-10-13 19:02:13 +0200

    Fixed UTF-8 abaut possible "validation" Crizz0 ;)
    Fixed UTF-8 abaut possible "validation" Crizz0 ;)
    This changes "About us" to “About us” on all files, verify this please, Spanish file updated, and this English file.
    
    Best regards.

--- language/en/common.php
+++ language/en/common.php
@@ -40,17 +40,17 @@ $lang = array_merge($lang, array(
 	'ABOUTUS_AND'					=> 'and',
 
 	'ACP_ABOUTUS_ENABLE'			=> 'Enable About us',
-	'ACP_ABOUTUS_ENABLE_EXPLAIN'	=> 'If the "About us" page is disabled, your board will not show a link in the footer to the "About us"-page. Nevertheless with the url "app.php/aboutus" you are able to visit the "About Us"-page. ',
+	'ACP_ABOUTUS_ENABLE_EXPLAIN'	=> 'If the “About us” page is disabled, your board will not show a link in the footer to the “About us”-page. Nevertheless with the url “app.php/aboutus” you are able to visit the “About us”-page. ',
 	'ACP_ABOUTUS_INFO'				=> 'About us text',
-	'ACP_ABOUTUS_INFO_EXPLAIN'		=> 'You can edit the text which is displayed on the "About us"-page.',
+	'ACP_ABOUTUS_INFO_EXPLAIN'		=> 'You can edit the text which is displayed on the “About us”-page.',
 	'ACP_ABOUTUS_INFO_PREVIEW'		=> 'About us Preview',
 	'ACP_ABOUTUS_SETTINGS'			=> 'Settings',
-	'ACP_ABOUTUS_SETTINGS_EXPLAIN'	=> 'Here you are able to define the text of the "About us"-page and enable or disable the "About us"-page.',
+	'ACP_ABOUTUS_SETTINGS_EXPLAIN'	=> 'Here you are able to define the text of the “About us”-page and enable or disable the “About us”-page.',
 
 	'ACP_TERMS_OF_USE_ENABLE' 			=> 'Display terms of use',
-	'ACP_TERMS_OF_USE_ENABLE_EXPLAIN'	=> 'Displays a link to the terms of use on the "About us"-page.',
+	'ACP_TERMS_OF_USE_ENABLE_EXPLAIN'	=> 'Displays a link to the terms of use on the “About us”-page.',
 	'ACP_PRIVACY_ENABLE' 				=> 'Display privacy policy',
-	'ACP_PRIVACY_ENABLE_EXPLAIN'		=> 'Displays a link to the privacy policy on the "About us"-page.',
+	'ACP_PRIVACY_ENABLE_EXPLAIN'		=> 'Displays a link to the privacy policy on the “About us”-page.',
 
 	'DESCRIPTION_PRIVACY'			=> 'You can see the privacy policy here:',
 	'DESCRIPTION_TERMS_OF_USE'		=> 'You can see the terms of use here:',
