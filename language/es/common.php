78d2b2d541ca634fb712836cdef32196a9fc35cd
phpBB España <thekuka@gmail.com>
2015-10-13 18:57:27 +0200

    Spanish translation updated.
    Spanish translation updated.

--- language/es/common.php
+++ language/es/common.php
@@ -2,8 +2,10 @@
 /**
 *
 * @package phpBB Extension - Crizzo About us - Spanish
-* @copyright (c) 2014 Crizzo - www.crizzo.de
+* @copyright (c) 2014-2015 Crizzo - www.crizzo.de
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+* Spanish Translation: ThE KuKa ( Raúl Arroyo ) - http://www.phpbb-es.com
 *
 */
 
@@ -17,24 +19,40 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
 $lang = array_merge($lang, array(
 	'ABOUTUS'						=> 'Sobre nosotros',
 	'ABOUTUS_SETTINGS'				=> 'Ajustes de Sobre nosotros',
 	'ABOUTUS_UPDATED'				=> 'Sobre nosotros actualizado correctamente.',
 	'ABOUTUS_AND'					=> 'y',
 
-	'ACP_ABOUTUS_ENABLE' => 'Habilitar Sobre nosotros',
-	'ACP_ABOUTUS_ENABLE_EXPLAIN' => 'Si la página "Sobre nosotros" esta deshabilitada, su foro no mostrará el enlace en el pie de página hacia la página "Sobre nosotros". Sin embargo con la URL "app.php/aboutus" si se podrá visitar la página "Sobre nosotros".',
+	'ACP_ABOUTUS_ENABLE' 			=> 'Habilitar Sobre nosotros',
+	'ACP_ABOUTUS_ENABLE_EXPLAIN' 	=> 'Si la página “Sobre nosotros” esta deshabilitada, su foro no mostrará el enlace en el pie de página hacia la página “Sobre nosotros”. Sin embargo con la URL “app.php/aboutus” si se podrá visitar la página “Sobre nosotros”.',
 	'ACP_ABOUTUS_INFO'				=> 'Texto de Sobre nosotros',
-	'ACP_ABOUTUS_INFO_EXPLAIN'		=> 'Puede editar el texto que será mostrado en la página Sobre nosotros.',
+	'ACP_ABOUTUS_INFO_EXPLAIN'		=> 'Puede editar el texto que será mostrado en la página “Sobre nosotros”.',
 	'ACP_ABOUTUS_INFO_PREVIEW'		=> 'Vista previa de Sobre nosotros',
 	'ACP_ABOUTUS_SETTINGS'			=> 'Ajustes de Sobre nosotros',
-	'ACP_ABOUTUS_SETTINGS_EXPLAIN'	=> 'Aquí puede definir el texto de la pa´gina "Sobre nosotros" y habilitar o deshabilitar la página "Sobre nosotros".',
+	'ACP_ABOUTUS_SETTINGS_EXPLAIN'	=> 'Aquí puede definir el texto de la página “Sobre nosotros” y habilitar o deshabilitar la página “Sobre nosotros”.',
 
 	'ACP_TERMS_OF_USE_ENABLE' 			=> 'Mostrar Condiciones de uso',
-	'ACP_TERMS_OF_USE_ENABLE_EXPLAIN'	=> 'Mostrar un enlace a las Condiciones de uso en la página Sobre nosotros.',
+	'ACP_TERMS_OF_USE_ENABLE_EXPLAIN'	=> 'Mostrar un enlace a las Condiciones de uso en la página “Sobre nosotros”.',
 	'ACP_PRIVACY_ENABLE' 				=> 'Mostrar Política de privacidad',
-	'ACP_PRIVACY_ENABLE_EXPLAIN'		=> 'Mostrar un enlace a la Política de privacidad en la página Sobre nosotros.',
+	'ACP_PRIVACY_ENABLE_EXPLAIN'		=> 'Mostrar un enlace a la Política de privacidad en la página “Sobre nosotros”.',
 
 	'DESCRIPTION_PRIVACY'			=> 'Puede ver la Política de privacidad aquí:',
 	'DESCRIPTION_TERMS_OF_USE'		=> 'Puede ver las Condiciones de uso aquí:',
