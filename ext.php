c1420ce2cad5b777f8a6b98f991b80dcc4f721b3
Christian Schnegelberger <mail@crizzo.de>
2017-03-11 22:50:08 +0100

    Adds tests to 2.1.x

---
+++ ext.php
@@ -0,0 +1,32 @@
+<?php
+/**
+*
+* About us extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2014-2017 Crizzo <http://www.crizzo.de>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace crizzo\aboutus;
+
+/**
+* @ignore
+*/
+
+class ext extends \phpbb\extension\base
+{
+	/**
+	 * Enable extension if phpBB minimum version requirement is met
+	 *
+	 * Requires phpBB 3.2.0 due to usage of new language-include and font-awesome icons.
+	 *
+	 * @return bool
+	 * @access public
+	 */
+	public function is_enableable()
+	{
+		$config = $this->container->get('config');
+		return phpbb_version_compare($config['version'], '3.2.0', '>=');
+	}
+}
