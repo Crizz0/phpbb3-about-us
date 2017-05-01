527bba898a208c0057a8808d7a135b44fa43abf4
Christian Schnegelberger <mail@crizzo.de>
2017-05-01 20:02:58 +0200

    Feedback from validation of 2.1.1 (part2)

--- acp/acp_aboutus_module.php
+++ acp/acp_aboutus_module.php
@@ -108,14 +108,19 @@ class acp_aboutus_module
 			'TERMS_OF_USE'					=> $config['acp_aboutus_termsofuse_enable'],
 			'PRIVACY'						=> $config['acp_aboutus_privacy_enable'],
 			'ABOUTUS_ENABLE'				=> $config['acp_aboutus_enable'],
+			
 			'S_BBCODE_DISABLE_CHECKED'		=> !$aboutus_edit['allow_bbcode'],
 			'S_SMILIES_DISABLE_CHECKED'		=> !$aboutus_edit['allow_smilies'],
 			'S_MAGIC_URL_DISABLE_CHECKED'	=> !$aboutus_edit['allow_urls'],
+			
 			'BBCODE_STATUS'					=> $language->lang('BBCODE_IS_ON', '<a href="' . $phpbb_container->get('controller.helper')->route('phpbb_help_bbcode_controller') . '">', '</a>'),
 			'SMILIES_STATUS'				=> $language->lang('SMILIES_ARE_ON'), 
 			'IMG_STATUS'					=> $language->lang('IMAGES_ARE_ON'),
 			'FLASH_STATUS'					=> $language->lang('FLASH_IS_ON'),
 			'URL_STATUS'					=> $language->lang('URL_IS_ON'),
+
+			'U_ACTION'						=> $this->u_action,
+
 			'S_BBCODE_ALLOWED'				=> true,
 			'S_SMILIES_ALLOWED'				=> true,
 			'S_BBCODE_IMG'					=> true,
