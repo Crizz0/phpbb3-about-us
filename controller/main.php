d66ec1fba25ee09227e6e87b09f41d647e072179
Christian Schnegelberger <mail@crizzo.de>
2020-01-18 16:31:20 +0100

    Update travis.yml for tests

--- controller/main.php
+++ controller/main.php
@@ -2,7 +2,7 @@
 /**
 *
 * About us extension for the phpBB Forum Software package.
-* 
+*
 * @copyright (c) 2014-2017 Crizzo <http://www.crizzo.de>
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
