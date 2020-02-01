13262045fb02ce440055b1623e1ca60b09e5b361
Christian Schnegelberger <mail@crizzo.de>
2020-02-01 17:27:19 +0100

    Bump version to 2.1.4-dev

--- acp/acp_aboutus_info.php
+++ acp/acp_aboutus_info.php
@@ -17,7 +17,7 @@ class acp_aboutus_info
 		return array(
 			'filename'	=> '\crizzo\aboutus\acp\acp_aboutus_module',
 			'title'		=> 'ACP_ABOUTUS_SETTINGS',
-			'version'	=> '2.1.3',
+			'version'	=> '2.1.4-dev',
 			'modes'		=> array(
 			'config_aboutus'	=> array('title' => 'ACP_ABOUTUS_SETTINGS', 'auth' => 'ext_crizzo/aboutus && acl_a_board', 'cat' => array('ACP_ABOUTUS_SETTINGS')),
 			),
