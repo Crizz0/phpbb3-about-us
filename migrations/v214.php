97a6d476bd7e0782ecb8a94ad667f6b727500a5d
Christian Schnegelberger <mail@crizzo.de>
2020-08-03 21:42:30 +0200

    Add whole new table to migration for ToU and PP texts

--- migrations/v214.php
+++ migrations/v214.php
@@ -22,31 +22,68 @@ class v214 extends \phpbb\db\migration\migration
 	public function update_data()
 	{
 		$data = array(
-				array('module.add', array(
-					'acp',
-					'ACP_ABOUTUS_SETTINGS',
-					array(
-							'module_basename'   => '\crizzo\aboutus\acp\acp_aboutus_module',
-							'modes'             => array('tou', 'privacy'),
-					),
-				)),
+					array('module.add', array(
+						'acp',
+						'ACP_ABOUTUS_SETTINGS',
+						array(
+								'module_basename'	=> '\crizzo\aboutus\acp\acp_aboutus_module',
+								'modes'				=> array('tou', 'privacy'),
+						),
+					)),
 				// Add new configs
 				array('config.add' , array('aboutus_tou_enable', false)),
 				array('config.add' , array('aboutus_pp_enable', false)),
 				array('config.add' , array('aboutus_tou_show_on_main', false)),
 				array('config.add' , array('aboutus_pp_show_on_main', false)),
 
-				// ToU
-				array('config_text.add', array('aboutus_tou', '')),
-				array('config_text.add', array('aboutus_tou_uid', '')),
-				array('config_text.add', array('aboutus_tou_bitfield', '')),
-				array('config_text.add', array('aboutus_tou_flags', OPTION_FLAG_BBCODE + OPTION_FLAG_SMILIES + OPTION_FLAG_LINKS)),
-				// Privacy policy
-				array('config_text.add', array('aboutus_pp', '')),
-				array('config_text.add', array('aboutus_pp_uid', '')),
-				array('config_text.add', array('aboutus_pp_bitfield', '')),
-				array('config_text.add', array('aboutus_pp_flags', OPTION_FLAG_BBCODE + OPTION_FLAG_SMILIES + OPTION_FLAG_LINKS)),
+
+				'add_tables'	=>array(
+					// Terms of Use
+					$this->table_prefix . 'aboutus_tou' => array(
+								'COLUMNS'       => array(
+									'tou_id'						=> array('UINT', null, 'auto_increment'),
+									'tou_title'						=> array('VCHAR:200', ''),
+									'lang_iso'						=> array('VCHAR:30', ''),
+									'tou_content'					=> array('MTEXT_UNI', ''),
+									'tou_contant_bbcode_uid'		=> array('VCHAR:255', ''),
+									'tou_content_bbcode_bitfield'	=> array('VCHAR:255', ''),
+									'tou_content_bbcode_options'	=> array('UINT:11', 7),
+									'tou_content_allow_html'		=> array('BOOL', 0),
+								),
+							'PRIMARY_KEY'	=> 'tou_id',
+						),
+					// Privacy policy
+					$this->table_prefix . 'aboutus_pp' => array(
+						'COLUMNS'		=> array(
+							'pp_id'							=> array('UINT', null, 'auto_increment'),
+							'pp_title'						=> array('VCHAR:200', ''),
+							'lang_iso'						=> array('VCHAR:30', ''),
+							'pp_content'					=> array('MTEXT_UNI', ''),
+							'pp_contant_bbcode_uid'         => array('VCHAR:255', ''),
+							'pp_content_bbcode_bitfield'	=> array('VCHAR:255', ''),
+							'pp_content_bbcode_options'		=> array('UINT:11', 7),
+							'pp_content_allow_html'			=> array('BOOL', 0),
+						),
+						'PRIMARY_KEY'	=> 'pp_id',
+					),
+				),
 		);
 		return $data;
+	}
+
+	/**
+	 * Drop the aboutus table schema from the database
+	 *
+	 * @return array Array of table schema
+	 * @access public
+	 */
+	public function revert_schema()
+	{
+		return array(
+			'drop_tables'	=> array(
+				$this->table_prefix . 'aboutus_tou',
+				$this->table_prefix . 'aboutus_pp',
+			),
+		);
 	}
 }
